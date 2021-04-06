ecbcf4c8cc20f4736354fd124626f579a5849a2c
Julien Blatecky <julien.blatecky@tiralarc-grand-est.fr>
2021-04-06 23:32:19 +0200

    Configure PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: {
+    "postcss-import": {
+      path: ["node_modules"],
+    },
     tailwindcss: {},
-    autoprefixer: {},
+    "postcss-preset-env": { stage: 1 },
   },
-}
+};
