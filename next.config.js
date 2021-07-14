f9906e030411e93ae2864ac54ea264e1c95c059f
Julien Blatecky <julien.blatecky@tiralarc-grand-est.fr>
2021-07-14 22:17:59 +0200

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const path = require("path");
 
 module.exports = {
+  reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-  },
-  future: {
-    strictPostcssConfiguration: true,
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <Layout title="Accueil">
       <main>
         <h1 className="heading-1">
-          Comité Régional de Tir à l'Arc du Grand Est
+          Comité Régional de Tir à l&apos;Arc du Grand Est
         </h1>
       </main>
     </Layout>
