a1b5e024f063a1c2cf9087e25beaa5c88e62744c
Julien Blatecky <julien.blatecky@tiralarc-grand-est.fr>
2021-07-24 11:36:36 +0200

    Remove proweredByHeader

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   trailingSlash: true,
+  poweredByHeader: false,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
