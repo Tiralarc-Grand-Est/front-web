b26c8a710c8750a12e2e86d9381728d32882974a
Julien Blatecky <julien.blatecky@tiralarc-grand-est.fr>
2021-07-25 20:39:58 +0200

    Add legals

---
+++ pages/mentions-legales.tsx
@@ -0,0 +1,59 @@
+import SimpleLayout from "../components/layout/SimpleLayout";
+
+export default function MentionsLegales() {
+  return (
+    <SimpleLayout
+      title="Mentions légales"
+      description="Mentions légales pour le site du Comité Régional de Tir à l'Arc du Grand Est"
+      mainClassName="prose"
+    >
+      <h1>Mentions légales</h1>
+
+      <h2>Comité Régional de Tir à l&#39;Arc du Grand Est</h2>
+
+      <p>
+        Organe déconcentré de la Fédération Française de Tir à l&#39;Arc (FFTA)
+      </p>
+
+      <p>
+        Association de loi 1901
+        <br />
+        Enregistrée auprès de la Préfecture de Meurthe-et-Moselle sous le
+        n°W543009328
+      </p>
+
+      <p>
+        Siret : <strong>82401205800015</strong>
+        <br />
+        APE : <strong>9312Z</strong>
+      </p>
+
+      <p>
+        Siège social :
+        <br />
+        <strong>
+          Maison régionale des sports
+          <br />
+          13 rue Jean Moulin - CS 70001
+          <br />
+          54510 Tomblaine
+        </strong>
+      </p>
+
+      <p>
+        Contact :{" "}
+        <a href="mailto:contact@tiralarc-grand-est.fr">
+          contact@tiralarc-grand-est.fr
+        </a>
+      </p>
+
+      <p>
+        Responsable légal : Suzanne Wrona (
+        <a href="mailto:presidente@tiralarc-grand-est.fr">
+          presidente@tiralarc-grand-est.fr
+        </a>
+        )
+      </p>
+    </SimpleLayout>
+  );
+}
